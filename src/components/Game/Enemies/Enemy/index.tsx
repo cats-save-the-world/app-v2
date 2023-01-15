a4c7c03d7fdba4e3e06f81ef9e047fce13e6c65f
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-01-15 22:54:13 +0600

    add enemy types

--- src/components/Game/Enemies/Enemy/index.tsx
+++ src/components/Game/Enemies/Enemy/index.tsx
@@ -1,22 +1,24 @@
 import { FC } from "react";
+import classNames from "classnames";
 import style from "./style.module.css";
 
 interface IProps {
   angle: number;
   distance: number;
+  type: string;
 }
 
-const Enemy: FC<IProps> = ({ angle, distance }) => {
+const Enemy: FC<IProps> = ({ angle, distance, type }) => {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
       <div
-        className="transition-[padding]"
+        className="transition-[padding,transform] duration-300"
         style={{
           transform: `rotate(${angle}deg)`,
           paddingBottom: `${distance}px`,
         }}
       >
-        <div className={style.enemy}></div>
+        <div className={classNames(style.enemy, style[type])}></div>
       </div>
     </div>
   );
