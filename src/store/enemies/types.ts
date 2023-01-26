2d07732f29f2ae6c9082135fbddf6f0f68f73392
Ruslan <r.kalashnikov@aviata.team>
2023-01-26 17:05:26 +0600

    feat: add heal enemy

--- src/store/enemies/types.ts
+++ src/store/enemies/types.ts
@@ -2,7 +2,7 @@ export interface IEnemy {
   id: string;
   angle: number;
   distance: number;
-  type: "simple" | "heavy" | "light" | "twisted";
+  type: "simple" | "heavy" | "light" | "twisted" | "heal";
   score: number;
   alive: boolean;
 }
