61aefe78eee998e78dcdea42113bf0b9e3ac35e9
Ruslan <r.kalashnikov@aviata.team>
2023-01-26 17:16:02 +0600

    fix: rename heal enemy to healing

--- src/store/enemies/types.ts
+++ src/store/enemies/types.ts
@@ -2,7 +2,7 @@ export interface IEnemy {
   id: string;
   angle: number;
   distance: number;
-  type: "simple" | "heavy" | "light" | "twisted" | "heal";
+  type: "simple" | "heavy" | "light" | "twisted" | "healing";
   score: number;
   alive: boolean;
 }
