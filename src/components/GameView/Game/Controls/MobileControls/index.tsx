4b616ce7a7c6e61576a8bdd6aeb44fb87fcfd69d
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-02-04 20:32:38 +0600

    split controls

---
+++ src/components/GameView/Game/Controls/MobileControls/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import Button from "./Button";
+
+interface IProps {
+  handleLeft: () => void;
+  handleRight: () => void;
+  handleStop: () => void;
+}
+
+const MobileControls: FC<IProps> = ({
+  handleLeft,
+  handleRight,
+  handleStop,
+}) => {
+  return (
+    <div className="fixed pb-4 px-4 h-[140px] bottom-0 left-0 right-0 grid grid-cols-2 gap-4">
+      <Button onTouchStart={handleLeft} onTouchEnd={handleStop} left />
+      <Button onTouchStart={handleRight} onTouchEnd={handleStop} />
+    </div>
+  );
+};
+
+export default MobileControls;
